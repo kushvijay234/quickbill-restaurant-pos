14107aa39b40acae75ea5f0eff8e59c165410c3a
Vijay Kushwaha <kushwahavijay234@gmail.com>
2025-10-03 22:57:36 +0530

    menu item UI

--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -34,8 +34,8 @@ const MenuItem: React.FC<MenuItemProps> = ({ item, currency, onAddToOrder, onEdi
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>
               </button>
         </div>
-      <img src={item.imageUrl} alt={item.name} className="w-full h-40 object-cover" />
-      <div className="p-4 flex flex-col justify-between h-40">
+      <img src={item.imageUrl} alt={item.name} className="w-full h-20 object-cover" />
+      <div className="p-4 flex flex-col justify-between h-30">
         <div>
           <h3 className="font-semibold text-lg text-gray-800 dark:text-gray-100">{item.name}</h3>
           <p className="text-gray-600 dark:text-gray-300 text-md font-bold mt-1">
